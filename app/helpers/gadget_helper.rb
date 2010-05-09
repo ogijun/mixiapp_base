64e65f6cb03d130e67e2639fc252c0c121a2b9ee
Junya Ogino <ogijun@gmail.com>
2010-05-09 15:45:20 +0900

    Stopped to use paginating_find.

--- app/helpers/gadget_helper.rb
+++ app/helpers/gadget_helper.rb
@@ -157,48 +157,6 @@ module GadgetHelper
   def mobile_gadget_url_for(options, with_transsid = true)
     @controller.send(:mobile_gadget_url_for, options, with_transsid)
   end
-
-  def mobile_gadget_paginating_links(paginator, options = {}, html_options = {})
-    name = options[:name] || PaginatingFind::Helpers::DEFAULT_OPTIONS[:name]
-    params = (options[:params] || PaginatingFind::Helpers::DEFAULT_OPTIONS[:params]).clone
-    
-    mobile_gadget_paginating_links_each(paginator, options) do |n|
-      params[name] = n
-      mobile_gadget_link_to(n, params, html_options)
-    end
-  end
-  
-  def mobile_gadget_paginating_links_each(paginator, options = {})
-    return if paginator.last_page==paginator.first_page
-    
-    options = PaginatingFind::Helpers::DEFAULT_OPTIONS.merge(options)
-    
-    window = ((paginator.page - options[:window_size] + 1)..(paginator.page + options[:window_size] - 1)).select {|w| w >= paginator.first_page && w <= paginator.last_page }
-    
-    html = ''
-    
-    if options[:always_show_anchors] && !window.include?(paginator.first_page)
-      html << yield(paginator.first_page)
-      html << ' ... ' unless window.first - 1 == paginator.first_page
-      html << ' '
-    end
-    
-    window.each do |p|
-      if paginator.page == p && !options[:link_to_current_page]
-        html << p.to_s
-      else
-        html << yield(p)
-      end
-      html << ' '
-    end
-    
-    if options[:always_show_anchors] && !window.include?(paginator.last_page)
-      html << ' ... ' unless window.last + 1 == paginator.last_page
-      html << yield(paginator.last_page)
-    end
-    
-    html
-  end
   
   protected
   def request_function(options, function_name)
